c3aac8a18a8974cda34a75c908a5a061e83f533c
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-09-27 14:46:53 +0000

    2024 - 27th - september

--- Assignments September 2024/MSOE-13-#1.rb
+++ Assignments September 2024/MSOE-13-#1.rb
@@ -1,8 +1,16 @@
-puts "First load is"
-one = gets.chomp
-puts "Second load is"
-two = gets.chomp 
-puts "Third load is"
-three = gets.chomp
-puts "Volatage source?"
-four = gets.chomp 
+puts "First resistence is"
+one = gets.chomp.to_f
+one = one / 100000
+puts "Second resistence is"
+two = gets.chomp.to_f
+two = two / 100000
+puts "Third resistence is"
+three = gets.chomp.to_f
+three = three / 100000
+puts "Volatage: "
+four = gets.chomp.to_f
+
+resistence = one + two + three
+r = four / resistence
+puts "The resistence is: " + r.to_s 
+
--- Assignments September 2024/Prog72t.rb
+++ Assignments September 2024/Prog72t.rb
@@ -5,5 +5,10 @@ b = gets.chomp.to_f
 a = a / 100
 b = b / 100
 c = a - b
+if c > 0
+    c = c
+elsif c < 0
+    c = c * -1
+end
 
-puts "Time difference is: " + c.to_s 
+puts "Time difference is: " + c.to_s + " Hours"
