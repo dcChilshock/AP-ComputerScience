a58ab2da8a671403c76d39d083210b7b201e2779
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-10-15 15:31:43 +0000

    10 - 15 - 2024

--- Assignments-October-2024/Prog122dForLoop.rb
+++ Assignments-October-2024/Prog122dForLoop.rb
@@ -1,3 +1,13 @@
-puts "enter a num between -12 and 16"
-var = gets.chomp
-num = ((var**6)-()-()+()+((1596*var)**2))-(1380(var))-(2800))
+var = -12
+x = 0
+array = [] 
+while var < 17
+    num = (var**6)-(3*var**5)-(93*var**4)+(87*var**3)+(1596*var**2)-(1380*var) - (2800)
+    array.push(num.to_s)
+    x = x + 1
+    var = var + 1
+end
+
+for array in array do
+  puts array
+end
