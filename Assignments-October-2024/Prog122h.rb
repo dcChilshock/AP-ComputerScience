887d74fa87fca5b16a6cc15928e120f49d763ba9
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-10-16 15:48:41 +0000

    10 - 16 - 2024

---
+++ Assignments-October-2024/Prog122h.rb
@@ -0,0 +1,19 @@
+num = 0.0
+square = 0.0
+root = 0.0
+cube = 0.0
+fourth = 0.0
+puts "Num    Square    Root    Cube    Fourth."
+x = 0
+
+for a in 1..20 do
+    x = x + 1
+    num = x
+    square = x * x
+    root = (Math.sqrt(x)).round(4)
+    cube = x * x * x
+    fourth = (Math.exp(Math.log(x)/4.0)).round(4)
+    a = num.to_s+"      "+square.to_s+"        "+root.to_s+"       "+cube.to_s+"       "+fourth.to_s+"."
+    print a
+    print "\n"
+end 
