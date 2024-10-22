30d411a4e8fc760c14f2c85f8d1934d1159e7917
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-10-22 15:32:24 +0000

    10/22/2024

--- Assignments-October-2024/MSOE-16-#4.rb
+++ Assignments-October-2024/MSOE-16-#4.rb
@@ -1,16 +1,15 @@
 puts "Insert a number between 1-10,000"
 n = gets.chomp.to_i
-a = n
-p = 1
-greatest common divisor
-gcd(a , b)
-Φ
-# Initialize : result = n
-#2) Run a loop from 'p' = 2 to sqrt(n), do following for every 'p'.
-#   a) If p divides n, then
-#    Set: result = result * (1.0 - (1.0 / (float) p));
-#    Divide all occurrences of p in n.
-#    3) Return result
-for i in 1..n
-    m = a * (1 - 1/p)
-    p = p + 1
+#find prime factors of a nuumber so 10
+array = []
+p = 2
+until n == 3
+    n = n / p
+    array.push(n.to_s)
+    if (n / p) % 1 == 0
+        p = p
+    elsif
+        p = p + 1
+    end
+end
+print array
