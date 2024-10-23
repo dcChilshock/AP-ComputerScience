ea72f37c5cf0c266c8b5dd7d0b88303c20cb3131
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-10-23 15:32:00 +0000

    10/23/2024

--- Assignments-October-2024/MSOE-16-#4.rb
+++ Assignments-October-2024/MSOE-16-#4.rb
@@ -3,13 +3,27 @@ n = gets.chomp.to_i
 #find prime factors of a nuumber so 10
 array = []
 p = 2
-until n == 3
+o = 0
+while o != 1
     n = n / p
-    array.push(n.to_s)
-    if (n / p) % 1 == 0
+    array.push(p.to_s)
+    if n == Integer.superclass
         p = p
-    elsif
+    elsif n == Float.superclass
+        n = n * p
         p = p + 1
+        array.pop
     end
+    if p == 10
+        o = 1
+    else
+        o = 0
+    end
+    print array
 end
-print array
+
+for array in array do
+    puts array
+  end
+#1.kind_of? Integer
+#1.5.kind_of? Float
