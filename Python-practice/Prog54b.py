030a3824fe1fb3d576848079acab57c873e82cd9
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-11-18 19:45:06 +0000

    11/18/2024/1

---
+++ Python-practice/Prog54b.py
@@ -0,0 +1,10 @@
+val1 = input("Enter value 1: ")
+val2 = input("Enter value 2: ")
+val3 = input("Enter value 3: ")
+val4 = input("Enter value 4: ")
+
+sum = val1 + val2 + val3 + val4
+av = sum / 4
+
+print(str(sum))
+print(str(av))
