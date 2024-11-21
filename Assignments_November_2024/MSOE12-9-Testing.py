4f5d73e3fb700fa585c76476679c71e9df2baacb
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-11-21 18:46:21 +0000

    11/21/2024/4

--- Assignments_November_2024/MSOE12-9-Testing.py
+++ Assignments_November_2024/MSOE12-9-Testing.py
@@ -1,11 +1,11 @@
 
 
-from MSOE129.py import reading
+from MSOE129 import reading
 
 array = ["XLVII","IX","XC","I","CXI","MCCLXXIII","MMMDDDCCCLLLXXXVVVIII","CDLX","DCIII","LXXIX"]
 
-for i in range(0,10):       #1   #110  #1273        #4998                 #460   #
-    string = array[i]
+for i in range(0,10):       #1   #110   #1273        #4998                 #460
+    string = array[i]            #Actually 111 they got it wrong on the document.
     reading(string)
 
 #/workspaces/AP-ComputerScience/Assignments-November-2024/MSOE12#9.py
--- Assignments_November_2024/MSOE129.py
+++ Assignments_November_2024/MSOE129.py
@@ -11,7 +11,6 @@
 # Subtraction = smaller number before larger number
 # I (1) - X(10) = 9
 
-string = input("input roman numerals: ")
 def reading(a):
     presum = 0
     sum = 0
@@ -20,7 +19,6 @@ def reading(a):
     d = {
         'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,
          }
-    delete = {}
     length = len(a)
     for i in range(0,length): 
         if a[i] == "M":
@@ -38,22 +36,15 @@ def reading(a):
         elif a[i] == "I":
             sum += d[a[i]]
         if d[a[i]] > previous and previous > 0:
-            print(str(sum))
-            print(str(presum))
-            print(str(previous))
-            print(str(d[a[i]]))
-            print("!!!!!!")
             sum -= (previous + d[a[i]])
-            print(str(sum))
             diff = d[a[i]] - previous
             sum += abs(diff)
-            print(str(sum))
-            print(str(diff))
-            #print(str(sum))
         else:
             sum = sum
         previous = d[a[i]]
-        presum = sum 
     print("This is the roman numeral. "+ a)
     print("This is the number in numeral form. "+str(sum))
-reading(string)
+
+def call():
+    string = "Input roman numerals that follow all the rules: "
+    reading(string)
