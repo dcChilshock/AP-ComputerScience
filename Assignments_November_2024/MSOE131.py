1495a01496ff2aab4527b26cd4ce0ea3c935cfe4
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-12-05 15:44:04 +0000

    11/21/2024

--- Assignments_November_2024/MSOE131.py
+++ Assignments_November_2024/MSOE131.py
@@ -1,4 +1,24 @@
 
 def race(a,b):
+    if "car" in a:
+        a1 = "car"
+        b1 = "bus"
+    elif "bus" in a:
+        a1 = "bus"
+        b1 = "car"
     len1 = len(a)
     len2 = len(b)
+    if a[-1] == ".":
+        return print(b1 + " wins")
+    elif b[-1] == ".":
+        return print(a1 + " wins")
+    elif a[-1] == "." and b[-1] == ".":
+        return print("scratch")
+    else:
+        if len1 > len2:
+            return print(a1 + " wins")
+        elif len1 < len2:
+            return print(b1 + " wins")
+        elif len1 == len2:
+            return print("Tie")
+    
---
+++ Assignments_November_2024/MSOE132.py
@@ -0,0 +1,16 @@
+
+def alliteration(a):
+    len1 = len(a)
+    count = 0
+    previous = ""
+    mylist = [a[0],]
+    dic = {a[0]: 0,}
+    for i in range(1,len1):
+        current = a[0]
+        if previous == " ":
+            mylist.append(a[i])
+            dic.append(a[i]:i)   
+        else:
+
+    print mylist
+    print dic            
