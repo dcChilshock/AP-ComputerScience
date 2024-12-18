ff24ee611399f621a76e374c304b02d1a07fae65
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-12-18 16:48:29 +0000

    12/18/2024

---
+++ Assignments-December-2024/booksearch.rb
@@ -0,0 +1,6 @@
+book = {"Abhorson Trilogy"=>"Garth Nix","Acacia: The war with the Mein"=>"David Anthony Durham","Aegypt"=>"John Crowley",
+"Akata Witch"=>"Nnedi Okorafor","Akata Warrior"=>"Nnedi Okorafor","Aladore"=>"Henry Newbolt",
+"Alice's Adventures in Wonderland"=>"Lewis Carroll",""=>"",""=>"",""=>"",""=>""}
+
+author = {"Garth Nix"=>"1","David Anthony Durham"=>"2","David R. Slayton"=>"3","John Crowley"=>"4",
+"Nnedi Okorafor"=>"5","Henry Newbolt"=>"6","Lewis Carroll"=>"7",""=>"",""=>"",""=>"",""=>"", ""=>""}
