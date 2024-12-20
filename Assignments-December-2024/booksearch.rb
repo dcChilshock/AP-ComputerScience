285cf9e4cb3de6ec3b721dc81546050c5fcc1c8f
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2024-12-20 08:23:48 +0000

    12/19/2024/2

--- Assignments-December-2024/booksearch.rb
+++ Assignments-December-2024/booksearch.rb
@@ -1,28 +1,46 @@
 # This is the list of books for my libary project 2
 # ""=>"",
-book = {"Artemis Fowl"=>"Eoin Colfer",
-"Chimera"=>"John Barth",
-"Cats have no lord"=>"Will Shetterly",
-"Aegypt"=>"John Crowley",
-"Akata Witch"=>"Nnedi Okorafor","Akata Warrior"=>"Nnedi Okorafor",
-"Aladore"=>"Henry Newbolt",
-"Alice in Wonderland"=>"Lewis Carroll",
-"The tales of alvin maker"=>"Orson Scott Card","Harts Hope"=>"Orson Scott Card",
-"American gods"=>"Neil Gaiman","Coraline"=>"Neil Gaiman",
-"Beauty"=>"Robin McKinley",
+
+list = ["American gods","Artemis Fowl","Alice in Wonderland","Aladore", "Aegypt","Akata Witch",
+"Akata Warrior","Beauty","Chimera","Cats have no lord","Coraline","City of Bones",
+"Harts Hope","Prince Caspian: The Return to Narnia",
+"The tales of alvin maker","The Lion, The witch, and the wardrobe","The Voyage of the Dawn Treader",
+"The Silver Chair","The Horse and his Boy","The Magicians Nephew","The Last battle",
+"The Death of the Necromancer","The Fall of Iie-Rien","The Element of fire","The Dark Tower:The gunsligner",
+"The Little Sisters of Eluria", "The Dark Tower II: The Drawing of the Three","The Dark Tower III: The Waste Lands",
+"The Dark Tower IV: Wizard and Glass","The Dark Tower: The Wind through the Keyhole",
+"The Dark Tower V: Wolves of the Dalla","The Dark Tower VI: Song of Susannah","The Dark Tower VI: The Dark Tower",
+"The Eyes of the Dragon","Discworld","Word & Void","The Genesis of Shannara",
+ "First King of Shannara","The Sword of Shannara","The Heritage of Shannara",
+ "The Voyage of the Jerle Shannara","High Druid of Shannara","The Dark Legacy of Shannara",
+"The Defenders of Shannara","The Fall of Shannara","Wizard for Hire","Wizard by trade","Wizard at Large",
+"Wizard under Fire","Small Favor","Turn Coat","Changes","Ghost Story","Cold days",
+"Skin Game","Peace Talks","Battle Grounds","Elantris","The Fool on the hill",
+"Eragon","Eldest","Brisingr","Inheritance","Excalibur",
+"The Last Hero", "The Son of Neptune","The House of Hades",
+"The Blood of Olympus","The Red Pyramid","The Throne of Fire",
+"The Serpent's Shadow","The Lighting Theif",
+"The Sea of Monsters","The Titan's curse",
+"The Battle of the Labyrinth","The Last Olympian",
+"The Hobbit","The Fellowship of the Ring",
+"The Two Towers","The Return of the King",
+"The Lost tales","Howl's Moving Castle",
+"The Name of the Wind","The Wise Man's Fear",
+"The Doors of Stone","Legends & Lattes","The Poppy War"
+]
+book = {"Artemis Fowl"=>"Eoin Colfer","Chimera"=>"John Barth","Cats have no lord"=>"Will Shetterly","Aegypt"=>"John Crowley",
+"Akata Witch"=>"Nnedi Okorafor","Akata Warrior"=>"Nnedi Okorafor","Aladore"=>"Henry Newbolt","Alice in Wonderland"=>"Lewis Carroll","The tales of alvin maker"=>"Orson Scott Card",
+"Harts Hope"=>"Orson Scott Card","American gods"=>"Neil Gaiman","Coraline"=>"Neil Gaiman","Beauty"=>"Robin McKinley",
 "The Lion, The witch, and the wardrobe"=>"C.S. Lewis","Prince Caspian: The Return to Narnia"=>"C.S. Lewis",
 "The Voyage of the Dawn Treader"=>"C.S. Lewis","The Silver Chair"=>"C.S. Lewis","The Horse and his Boy"=>"C.S. Lewis",
-"The Magicians Nephew"=>"C.S. Lewis","The Last battle"=>"C.S. Lewis",
-"City of Bones"=>"Martha Wells","The Death of the Necromancer"=>"Martha Wells",
-"The Fall of Iie-Rien"=>"Martha Wells","The Element of fire"=>"Martha Wells",
+"The Magicians Nephew"=>"C.S. Lewis","The Last battle"=>"C.S. Lewis","City of Bones"=>"Martha Wells",
+"The Death of the Necromancer"=>"Martha Wells","The Fall of Iie-Rien"=>"Martha Wells","The Element of fire"=>"Martha Wells",
 "The Dark Tower:The gunsligner"=>"Stephen King","The Little Sisters of Eluria"=>"Stephen King",
 "The Dark Tower II: The Drawing of the Three"=>"Stephen King","The Dark Tower III: The Waste Lands"=>"Stephen King",
 "The Dark Tower IV: Wizard and Glass"=>"Stephen King","The Dark Tower: The Wind through the Keyhole"=>"Stephen King",
 "The Dark Tower V: Wolves of the Dalla"=>"Stephen King","The Dark Tower VI: Song of Susannah"=>"Stephen King",
-"The Dark Tower VI: The Dark Tower"=>"Stephen King","The Eyes of the Dragon"=>"Stephen King",
-"Discworld"=>"Terry Pratchett",
-"Word & Void"=>"Terry Brooks","The Genesis of Shannara"=>"Terry Brooks",
-"First King of Shannara"=>"Terry Brooks","The Sword of Shannara"=>"Terry Brooks",
+"The Dark Tower VI: The Dark Tower"=>"Stephen King","The Eyes of the Dragon"=>"Stephen King","Discworld"=>"Terry Pratchett",
+"Word & Void"=>"Terry Brooks","The Genesis of Shannara"=>"Terry Brooks","First King of Shannara"=>"Terry Brooks","The Sword of Shannara"=>"Terry Brooks",
 "The Heritage of Shannara"=>"Terry Brooks","The Voyage of the Jerle Shannara"=>"Terry Brooks",
 "High Druid of Shannara"=>"Terry Brooks","The Dark Legacy of Shannara"=>"Terry Brooks",
 "The Defenders of Shannara"=>"Terry Brooks","The Fall of Shannara"=>"Terry Brooks",
@@ -30,59 +48,122 @@ book = {"Artemis Fowl"=>"Eoin Colfer",
 "Wizard under Fire"=>"Jim Butcher","Small Favor"=>"Jim Butcher","Turn Coat"=>"Jim Butcher",
 "Changes"=>"Jim Butcher","Ghost Story"=>"Jim Butcher","Cold days"=>"Jim Butcher",
 "Skin Game"=>"Jim Butcher","Peace Talks"=>"Jim Butcher","Battle Grounds"=>"Jim Butcher",
-"Elantris"=>"Brandon Sanderson",
-"The Fool on the hill"=>"Matt Ruff",
+"Elantris"=>"Brandon Sanderson","The Fool on the hill"=>"Matt Ruff",
 "Eragon"=>"Christopher Paolini","Eldest"=>"Christopher Paolini","Brisingr"=>"Christopher Paolini",
-"Inheritance"=>"Christopher Paolini",
-"Excalibur"=>"Sanders Anne Laubenthal",
-"The Last Hero"=>"Rick Riordan","The Son of Neptune"=>"Rick Riordan","The House of Hades"=>"Rick Riordan",
+"Inheritance"=>"Christopher Paolini","Excalibur"=>"Sanders Anne Laubenthal","The Last Hero"=>"Rick Riordan",
+"The Son of Neptune"=>"Rick Riordan","The House of Hades"=>"Rick Riordan",
 "The Blood of Olympus"=>"Rick Riordan","The Red Pyramid"=>"Rick Riordan","The Throne of Fire"=>"Rick Riordan",
-"The Serpent's Shadow"=>"Rick Riordan","The Lighting Theif"=>"Rick Riordan",
-"The Sea of Monsters"=>"Rick Riordan","The Titan's curse"=>"Rick Riordan",
+"The Serpent's Shadow"=>"Rick Riordan","The Lighting Theif"=>"Rick Riordan","The Sea of Monsters"=>"Rick Riordan","The Titan's curse"=>"Rick Riordan",
 "The Battle of the Labyrinth"=>"Rick Riordan","The Last Olympian"=>"Rick Riordan",
 "The Hobbit"=>"J.R.R. Tolkien","The Fellowship of the Ring"=>"J.R.R. Tolkien",
 "The Two Towers"=>"J.R.R. Tolkien","The Return of the King"=>"J.R.R. Tolkien",
-"The Lost tales"=>"J.R.R. Tolkien",
-"Howl's Moving Castle"=>"Diana Wynn Jones",
-"The Name of the Wind"=>"Patrick Rothfuss","The Wise Man's Fear"=>"Patrick Rothfuss",
-"The Doors of Stone"=>"Patrick Rothfuss",
-"Legends & Lattes"=>"Travis Baldree",
-"The Poppy War"=>"R.F. Kuang"}
+"The Lost tales"=>"J.R.R. Tolkien","Howl's Moving Castle"=>"Diana Wynn Jones","The Name of the Wind"=>"Patrick Rothfuss",
+"The Wise Man's Fear"=>"Patrick Rothfuss","The Doors of Stone"=>"Patrick Rothfuss","Legends & Lattes"=>"Travis Baldree","The Poppy War"=>"R.F. Kuang"}
 
-author = ("Eoin Colfer","John Barth","Travis Baldree","John Crowley",
+author = ["Eoin Colfer","John Barth","Travis Baldree","John Crowley",
 "Nnedi Okorafor","Henry Newbolt","Lewis Carroll","Orson Scott Card",
 "Neil Gaiman","Robin McKinley","C.S. Lewis", "Martha Wells","Stephen King",
 "Terry Pratchett","Jim Butcher","Brandon Sanderson","Christopher Paolini",
 "Sanders Anne Laubenthal","Matt Ruff","Rick Riordan","J.R.R. Tolkien",
-"Diana Wynn Jones","Patrick Rothfuss","Terry Brooks","R.F. Kuang")
+"Diana Wynn Jones","Patrick Rothfuss","Terry Brooks","R.F. Kuang"]
 
 filename = "bookssaved.txt"
 
-def file_add(x)
-File.open(filename, "w") do |file|
-    file.close
+def book_add()
+   puts "Which book would you like to add to your list?"
+   puts "Use the books fullname"
+   d = gets.chomp
+   mach.push(d.to_s)
 end
 
+def file_add(x)
+   File.open(filename, "w") do |file|
+      #add file thing if I have time
+      file.close
+   end
+end
 puts "Do you Wish to start libarytracker3000?"
 puts "y or n."
 n = gets.chomp
-if n == "y":
+i = "1"
+def start(list,book,i)
+   mach = []
+   def book_add(mach)
+      puts "Which book would you like to add to your list?"
+      puts "Use the books fullname"
+      d = gets.chomp
+      mach.push(d.to_s)
+   end
    puts "What would you like to do."
    puts "1. Add Books to your list: "
    puts "2. Remove Books from your list: "
-   puts "3. Request Books to be added: "
-   puts "4. "
-   puts "5. Exit"
-   y = gets.chomp.to_string
+   puts "3. Exit"
+   y = gets.chomp.to_s
    if y == "1"
-      
+      a = list.length
+      v = 0
+      h = 10
+      puts " "
+      puts " "
+      for l in 1..a do
+         v += 1
+         puts l.to_s+". "+list[l]+". \n Author: "+book[list[l]]
+         if v == h
+            h = v + 10
+            puts "1. Next page."
+            if v > 10
+               puts "1-2. Back page."
+               puts "2. Choose a book."
+               puts "3. Return to main."
+            else
+               puts "2. Choose a book."
+               puts "3. Return to main."
+            end 
+            c = gets.chomp.to_s
+            if c == "1-2"
+               l = l - 10
+            elsif c == "3"
+               book_add(mach)
+               #puts "Which book would you like to add to your list?"
+               #d = gets.chomp
+               #mach.push(d.to_s)
+               break
+            elsif c == "4"
+               puts "Returning to main"
+               break
+            elsif c == "1"
+               puts "Continuing"
+            end
+         end
+      end
    elsif y == "2"
-
+      f = mach.length
+      if f > 0
+         for s in 1..f do
+            v += 1
+            puts s.to_s+". "+mach[s]+". \n Author: "+book[mach[s]]
+            if l == f
+               puts "Which book would you like to remove?"
+               puts "Give the numbered position of the book in your list, the first book is 1."
+               g = gets.chomp.to_i
+               g = g - 1
+               mach.delete_at(g)
+               break 
+            else
+               puts "hello"
+            end
+         end
+      end
    elsif y == "3"
-   
-   elsif y == "4"
-
-   elsif y == "5"
+      i = "2"
       "Done."
+   end
+end
+if n == "y"
+   while i == "1"
+      start(list,book,i)
+   end
 else
    puts "what are you here for then?"
+end
+   
