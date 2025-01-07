2f7a39c544c786268cc2431dc8d445bdd1dbe9df
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2025-01-07 16:08:41 +0000

    1/7/2025/0

---
+++ Assignments January 2025/408a.py
@@ -0,0 +1,20 @@
+# imports time, your sorting library, and your helper class
+
+def main():
+    try:
+        data = []
+        with open("data/prg408a.dat", 'r')as f:
+            lines = f.readlines()
+            for line in lines:
+                id, score = line.split(' ')
+                id = int(id)
+                score = int(score)
+                #Make helper class objects and add to data
+        #ToDo: your code here
+        ...
+    except Exception as e:
+        print("Error:",e)
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ Assignments January 2025/408b.py
@@ -0,0 +1,15 @@
+# import time and your searching library
+
+def main():
+    try:
+        data = []
+        with open("data/prog408b.dat", 'r') as f:
+            data = [int(line) for line in f]
+        # todo: your code here
+        ...
+    except Exception as e:
+        print("Error:",e)
+    pass
+
+if __name__ == "__main__":
+    main()
