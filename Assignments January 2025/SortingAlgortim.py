b7e5221a6af8f1d40de5ce0060a53d0730191141
dcChilshock <143834259+dcChilshock@users.noreply.github.com>
2025-01-17 16:30:33 +0000

    1/17/2025 0

--- Assignments January 2025/SortingAlgortim.py
+++ Assignments January 2025/SortingAlgortim.py
@@ -1,12 +1,12 @@
 
 
 
-
 class SortingAlgorthim:
     def __init__(self, id, score):
         self.id = id
         self.score = score
-    def id():
 
     def __lt__(self, other):
-    return self.score < other.score
+        return self.score < other.score
+
+    def id();
--- Assignments January 2025/SortingLibrarya.py
+++ Assignments January 2025/SortingLibrarya.py
@@ -1,1 +1,30 @@
 # bubble, selection, insertion
+
+class sorting:
+    def __init__(self, list):
+        self.list = list
+
+    def bubble(self): #add to here later
+        a = 0
+        length = len(list) 
+        current = list[a] # 0 is the largest, last is the lowest
+        for i in length:
+            if current < list[i]:
+                # [ 0 , 1 , 2 , 3, 4]
+                list[a] = i
+                list[i] = a
+                a = i
+            else:
+                pass
+        return list 
+    
+    def selection(self):
+        for i in length:
+            if list[i] > current:
+                pass
+            else:
+                
+    def insertion(self):
+
+    
+
