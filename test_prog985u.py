d105a64a39c5548aa1275dde19d15f53987114be
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-01-29 15:54:26 +0000

    1/29/2025

--- test_prog985u.py
+++ test_prog985u.py
@@ -17,9 +17,7 @@ class TestQuicksort(unittest.TestCase):
     def test_normal_case(self):
         input = [4,2,5,1,3]
         expected_output = [1,2,3,4,5]
-        sef.assertEqual(Quicksort.sort(input), expected_output)
-
-    #TODO the rest of the test cases
+        self.assertEqual(Quicksort.sort(input), expected_output)
 
     def test_performance_large_dataset(self):
         input = generate_large_list()
@@ -28,6 +26,32 @@ class TestQuicksort(unittest.TestCase):
         end_time = current_time
         #Quicksort has to take less than ten seconds when sorting 1 million items
         self.assertLess(end_time,start_time,10)
+    
+    def test_empty_case(self):
+        input = []
+        expected_output = []
+        self.assertEqual(Quicksort.sort(input),expected_output)
+
+    def test_single_case(self):
+        input = [1]
+        expected_output = [1]
+        self.assertEqual(Quicksort.sort(input),expected_output)
+    def test_identical_elements(self):
+        input = [5,5,5,5]
+        expected_output = [5,5,5,5]
+        self.assertEqual(Quicksort.sort(input),expected_output)
+
+
+    def test_negative_numbers(self):
+        input = [-3,-1,-4,-2]
+        expected_output = [-4,-3,-2,-1]
+        self.assertEqual(Quicksort.sort(input),expected_output)
+
+    def test_mixed_types(self):
+        input = [3.2,1.5,4.8,2.1]
+        expected_output = [1.5,2.1,3.2,4.8]
+        self.assertEqual(Quicksort.sort(input),expected_output)
+
 #python test_prog985u.py
 if __name__ == '__main__':
     sutie = unittest.TestLoader().loadTestsFromTestCase(TestQuicksort)
