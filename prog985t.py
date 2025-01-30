2590ed354eb719049fcb6a09034eaa2d0dfe039d
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-01-30 16:30:57 +0000

    1/30/2025/0 Update

--- prog985t.py
+++ prog985t.py
@@ -1,16 +1,39 @@
 class Mergesort:
     @staticmethod #sorts in ascending order. 
-    def mergesort(A):
+    def sort(A):
         if length(A) > 1:
             mid=length(A)/2
             L = A[0..mid-1]
             R = A[mid..length(A)]
-            mergesort(L)
-            mergesort(R)
-            merge(A,L,R)
+            Mergesort.sort(L)
+            Mergesort.sort(R)
+            Mergesort.merge(A,L,R)
         else if:
             
 
 
     @staticmethod
-    def merge(A):
+    def merge(A,L,R):
+        i = 0
+        j = 0
+        k = 0
+        while i < length[L] and j < length[R] do:
+            if L[i] < R[j]:
+                A[k] = L[i]
+                i = i + 1
+            else:
+                A[k] = R[j]
+                j = j + 1
+            k = k + 1
+        while i < length[L]:
+            A[k] = L[i]
+            i = i + 1
+            k = k + 1
+        while j < length[R]:
+            A[K] = R[j]
+            j = j + 1
+            k = k + 1
+
+
+
+                
--- test_prog985t.py
+++ test_prog985t.py
@@ -3,6 +3,11 @@ import unittest
 from prog985t import Mergesort
 from time import perf_counter as current_time
 
+#Normal cases with typical lists of numbers.
+#Edge cases like an empty list, a list with one element, and a list where all elements are identical.
+#Lists with negative numbers and mixed integer/float values.
+#Test for performance (e.g., time taken to sort) for large datasets.
+
 def generate_large_list():
     return [random.randint(0,10_000_000) for i in range(1_000_000)]
 
@@ -13,3 +18,9 @@ class TestMergesort(unittest.TestCase):
     def tearDown(self):
         t = current_time() - self.starTime
         print(f"{self.id()}: {t:6f}") #t:6f rounds it to the sixth decimal point
+
+    def test_normal_case(self):
+        input = [4,2,5,1,3]
+        expected_output = [1,2,3,4,5]
+        self.assertEqual(Mergesort.sort(input), expected_output)
+
