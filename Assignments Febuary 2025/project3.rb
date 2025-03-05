70167f69da8d614a92645ca83b8844f5ab7b877a
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-03-05 14:45:10 +0000

    3/5/2025/Update

--- Assignments Febuary 2025/project3.rb
+++ Assignments Febuary 2025/project3.rb
@@ -14,14 +14,22 @@ def convert(num, unit1, unit2):
     elsif a < b #this means its division.
         num / unit1[unit2]
 
-puts "Available unit conversions: Length, Mass, Time, Temperature"
+check = [length]
+
 #Length includes inch, foot, mile, kilometer, meter, centimeter
 length = {centimeter => 1,inch => 2,meter =>3, feet => 4, mile => 5,kilometer => 6,} #length dictionary
 
 inch = {centimeter => 2.54,feet => 12,meter => 39.37,mile => 63360,kilometer => 39370,}
 centimeter = {inch => 2.54,feet => 30.48,meter => 100,mile => 160900,kilometer => 100000,}
 feet = {centimeter => 30.48,inch => 12,meter => 3.281,mile =>5280,kilometer =>3281 ,}
-mile = {centimeter => ,feet => ,meter => ,inch => ,kilometer => ,}
-kilometer = {centimeter => ,feet => ,meter => ,mile => ,inch => ,}
-meter = {centimeter => ,feet => ,inch => 39.37,mile => ,kilometer => ,}
-temp name
+mile = {centimeter =>160934 ,feet =>5280 ,meter =>1609 ,inch =>63360 ,kilometer =>1.609 ,}
+kilometer = {centimeter =>100000 ,feet =>3280.84 ,meter =>1000 ,mile =>1.609 ,inch =>39370 ,}
+meter = {centimeter =>100 ,feet =>3.28 ,inch =>39.37,mile =>,kilometer => ,}
+
+def start():
+    puts "Available unit conversions, Length:More will be avaliable in time."
+    answer = gets.chomp
+    if answer in check
+        ""
+
+
