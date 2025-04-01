5441b48fe484c6b2bf51d46969334970cbdc7e85
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-04-01 15:31:09 +0000

    4/1.2025

--- Book_Rec.py
+++ Book_Rec.py
@@ -4,7 +4,6 @@ import numpy as np
 #headings in order 1: Index/Id, 2 title, 3 authors, 4 rating, 5 isbn, 6 isbn13, 7 language,  8 page_count, 
 #--9 ratings count, 10 publication, 11 publisher 
 
-df = pd.read_csv("/workspaces/AP-ComputerScience/data/books.csv",sep = ",", header = 0)
-
+df = pd.read_csv("data/books.csv",nrows=1000)
 print(df)
 
