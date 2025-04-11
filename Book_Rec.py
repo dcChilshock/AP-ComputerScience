4ba3acb9c317c7c033969d123446088aa08f6acc
Nolan Denson-Childers <143834259+dcChilshock@users.noreply.github.com>
2025-04-11 19:35:42 +0000

    update

--- Book_Rec.py
+++ Book_Rec.py
@@ -8,6 +8,20 @@ listrate = ["3"] # 3 stars or higher
 listread = []
 listyear = ["2000"]
 
+isbns = []
+titles = []
+subt = []
+auth = []
+genre = [] 
+link = [] 
+des = [] 
+year = []
+nump = []
+rate_av = []
+rate_co = []
+ranking = []
+books = []
+dicton = {}
 def start():
      #Possibly put intro text/lengthy explanations into a def
     #within a class(so inheriting it to every fucntion isnt so annoying at least I think it would fix that.)
@@ -74,86 +88,93 @@ def prefrence():
 def recommend(a,b,c,d):
     df = pd.read_csv("data/books.csv",header=0,nrows=1000)
 
+def score(book,listid,listgen,listauth,listrate,listyear,listread): #potentially add a database/csv file of user books read. but
+    # that would be a lot of wokr this late into the project.
+    # needs lists of userers prefrences
+    rt = {}
+    t = len(book)
+    r = len(listid)
+    score = 1000
+    ly = int(listyear[0])
+    if book[5] in listgen:
+            score += 250
+    else:
+        score -= 100
+    if book[4] in listauth:
+        score += 100
+    else:
+            score -= 50
+    if float(book[8]) < ly:
+            score -= 100
+    else:
+            score += 100
+    if float(book[9]) >= int(listrate[0]):
+            score += 150
+    else:
+            score -= 150
+    if book[2] in listread:
+            score -= 500
+    else:
+            score += 150
+    for x in range(0,t):
+         pass
+    return score
+def read():
+    with pd.read_csv("data/books.csv",iterator=True,chunksize=6) as reader:
+        diction = {}
+        listid = ["isbn13","isbn10","title","subtitle","authors","categories","thumbnail","description","published_year","average_rating","num_pages","ratings_count"]
+        nc = 0
+        f = 0
+        t = len(listid)
+        for chunk in reader:
+            x = 0
+            g = 0
+            nc += 1
+            for row in chunk.iterrows():
+                book = []
+                for x in range(0,t):
+                    x +=1
+                    if g >= t:
+                        g = 0
+                    ind = listid[g]
+                    if g == 0:
+                        isbns.append(chunk.at[f,ind])
+                    elif g == 2:
+                        titles.append(chunk.at[f,ind])
+                    elif g == 3:
+                        subt.append(chunk.at[f,ind])
+                    elif g == 4:
+                        auth.append(chunk.at[f,ind])
+                    elif g == 5:
+                        genre.append(chunk.at[f,ind])
+                    elif g == 6:
+                        link.append(chunk.at[f,ind])
+                    elif g == 7:
+                        des.append(chunk.at[f,ind])
+                    elif g == 8:
+                        year.append(chunk.at[f,ind])
+                    elif g == 9:
+                        nump.append(chunk.at[f,ind])
+                    elif g ==10:
+                        rate_av.append(chunk.at[f,ind])
+                    elif g ==11:
+                        rate_co.append(chunk.at[f,ind])
+                    book.append(chunk.at[f,ind])
+                    #add a dictionary function 
+                    #where you can store every score to a list or the isbns to the list itself.
+                    g += 1
+                points= score(book,listid,listgen,listauth,listrate,listyear,listread)
+                ranking.append(points)
+                book.append(points)
+                books.append(book)
+                f += 1
+            
+
+    def show(diction):
+
+         print("Here are youre Book recommendations")
+         t = len(isbns)
+         a = 1
 print("<Welcome to the magnificent book finder.>")
 print("<How can we Help you today?>") 
 
-with pd.read_csv("data/books.csv",iterator=True,chunksize=6) as reader:
-    listid = ["isbn13","isbn10","title","subtitle","authors","categories","thumbnail","description","published_year","average_rating","num_pages","ratings_count"]
-    nc = 0
-    f = 0
-    isbns = []
-    titles = []
-    subt = []
-    auth = []
-    genre = [] 
-    link = [] 
-    des = [] 
-    year = []
-    nump = []
-    rate_av = []
-    rate_co = []
-    t = len(listid)
-    for chunk in reader:
-        x = 0
-        g = 0
-        nc += 1
-        for row in chunk.iterrows():
-            book = []
-            for x in range(0,t):
-                x +=1
-                if g >= t:
-                    g = 0
-                ind = listid[g]
-                if g == 0:
-                    isbns.append(chunk.at[f,ind])
-                elif g == 2:
-                    titles.append(chunk.at[f,ind])
-                elif g == 3:
-                    subt.append(chunk.at[f,ind])
-                elif g == 4:
-                    auth.append(chunk.at[f,ind])
-                elif g == 5:
-                    genre.append(chunk.at[f,ind])
-                elif g == 6:
-                    link.append(chunk.at[f,ind])
-                elif g == 7:
-                    des.append(chunk.at[f,ind])
-                elif g == 8:
-                    year.append(chunk.at[f,ind])
-                elif g == 9:
-                    nump.append(chunk.at[f,ind])
-                elif g ==10:
-                    rate_av.append(chunk.at[f,ind])
-                elif g ==11:
-                    rate_co.append(chunk.at[f,ind])
-                book.append(chunk.at[f,ind]) #add a dictionary function 
-                #where you can store every score to a list or the isbns to the list itself.
-
-                #points=score(book,listid,listgen,listrate,listauth,listread)
-                g +=1
-            f += 1
-
-def score(book,listid,listgen,listauth,listrate,listyear): #potentially add a database/csv file of user books read. but
-    # that would be a lot of wokr this late into the project.
-    # needs lists of userers prefrences
-    t = len(book)
-    r = len(listid)
-    x = 0
-    score = 1000 #less than
-    ly = int(listyear)
-    for x in range(0,t):
-        cur = book[x]
-        if book[5] in listgen: #might need more sophisticated methods of comparing the two.
-            score += 250
-        else:
-            score -= 100
-        if book[4] in listauth:
-            score += 100
-        else:
-            score -= 50
-        if book[10] < ly:
-            score -=
-
-            
-
-        x += 1
