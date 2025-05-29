397b7d8a63839fa03d812ac21a19d4778bcbe31f
Nolan Denson-Childers <densonchilders123@gmail.com>
2025-05-29 10:31:23 -0500

    update

--- assembly/bigmip4.asm
+++ assembly/bigmip4.asm
@@ -12,8 +12,26 @@
 t: .asciiz "Total is: "
 .text
 main:
-
+li $t0, 40 #variable is set to 40
+li $t1,100
+li $t2, 20 #add to t0 4 times
+li $t3,0 #cound
+li $t4,1 
+li $t5,4
+loop:
+add $t0,$t2,$t0
+add $t3,$t4,$t3
+bgt $t0,$t1,subt
+blt $t3,$t5,loop
+subt:
+sub $t0,$t0,$t1
+la $a0,t
+li $v0,4
+syscall
+move $a0,$t0
+li $v0,1
+syscall 
 exit:
-li $v0,10
+li $v0,10 #exit
 syscall
 
